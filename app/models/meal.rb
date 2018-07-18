6cf989d38d58b72a3eddaa1a3823a5194f1ab177
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-18 13:10:41 -0400

    Added filtering in meals index page by calorie count and by meal category

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,12 @@ class Meal < ApplicationRecord
   has_many :orders
   has_many :trips, through: :orders
   validates :name, :description, :price, presence: true
+
+  def self.low_cal
+    where("calorie_count <= ?", 350)
+  end
+
+  def self.by_category(category)
+    where(category: category)
+  end
 end
