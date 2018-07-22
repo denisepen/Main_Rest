dbdc27c02c85812788e8e5956efe9dbe3b74bb60
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-22 18:30:41 -0400

    added flash herlper for app.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
       (link_to('log in with Facebook', '/auth/facebook'))
     else
       (link_to current_user.named, user_path(current_user)) + " | " +
-      (link_to "Total: #{number_to_currency(session[:total])}", trip_path(session[:trip_id]) if session[:trip_id]) + " | " +
+      (link_to "total: #{number_to_currency(session[:total])}", trip_path(session[:trip_id]) if session[:trip_id]) + " | " +
       (link_to "log out", logout_path , method: 'post')
     end
   end
@@ -20,6 +20,25 @@ module ApplicationHelper
        (link_to "Customer Orders", trips_path)
        end
   end
+
+  def flash_helper
+    flash.map do |name, msg|
+      if name.include? 'notice'
+        content_tag(:div, msg, class: "notice")
+      else
+       content_tag(:div, msg, class: "alert alert-info")
+       end
+     end.join.html_safe
+  end
+
+# orig. flash message in app.html.erb
+  # <% flash.each do |name, msg| %>
+  # <% if name.include? 'notice' %>
+  #   <%= content_tag :div, msg, class: "notice" %>
+  #   <% else %>
+  #   <%= content_tag :div, msg, class: "alert alert-info" %>
+  #   <% end %>
+  # <% end %>
 
 
   # #<% if current_user && current_user.admin %>
