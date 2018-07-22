faafea33c5a93e69cbb2a8ad810b195d6a9ee3ca
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-22 17:56:56 -0400

    added admin helper to view helper for app.html.erb admin items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
   def login_helper
-
     if !logged_in?
       (link_to "sign up", new_user_path, method: 'get') + " | " +
       (link_to "sign in", signin_path , method: 'get') + " | " +
@@ -11,11 +10,26 @@ module ApplicationHelper
       (link_to "Total: #{number_to_currency(session[:total])}", trip_path(session[:trip_id]) if session[:trip_id]) + " | " +
       (link_to "log out", logout_path , method: 'post')
     end
-
-
-
-
   end
+
+
+  def admin_items
+    if logged_in? && is_admin?
+       # 'ADMIN' + " | " +
+       (link_to "ADMIN  |  Add New Meal", new_meal_path) + " |  "  +
+       (link_to "Customer Orders", trips_path)
+       end
+  end
+
+
+  # #<% if current_user && current_user.admin %>
+  #   <li class='nav'><%= "ADMIN"  %></li>
+  # <% end %>
+  # <%# if current_user %>
+  #   <% if is_admin? %>
+  #     <li><%= link_to "Add New Meal", new_meal_path %></li>
+  #     <li><%= link_to "Customer Orders", trips_path %></li>
+  #   <% end %>
 
   # <% if !logged_in? %>
   #   <li><%=  link_to "sign up", new_user_path, method: 'get' %></li>
