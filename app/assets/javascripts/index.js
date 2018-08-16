6dd7dc864c51e4ec3d0a18778a64e594be98598c
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-16 15:03:19 -0400

    created getMeals function to add meals to Meals index page (onclick)

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,18 @@
 
 
 function getMeals(){
-   $.get("/meals.json"), function(response){
-      console.log("Name: " + response["name"], "Price: " + response["price"] )
-  }
-}
+   $.get("/meals.json", function(response){
+         // console.log("Response: ", response["0"])
+         mealsContainer = document.getElementById("meals-list");
+        //
+        // var mealsList;
+
+
+           for (var i=0; i<response.length; i++){
+            // $("#meals-list ").after("<td>" + response[i]["name"] + "</td><td>" + response[i]["price"] + "</td><br>")
+            var meals;
+              meals += "<tr><td>" + response[i]["name"] + "</td><td>" + "$" + response[i]["price"] + "</td></tr>"
+           }
+           $("#meals-list").after().append(meals)
+        });
+      }
