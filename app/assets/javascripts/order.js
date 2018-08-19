07017826baf28e787cd8fa3a8095254d83118fff
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-18 21:07:39 -0400

    added style display none to the menu meals description to make toggle work correctly

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,17 +1,20 @@
-// plan: On the users show page, when "orders" is moused over or clicked the customers list of orders is rendered
+
 // $("document").ready(getuserOrders());
 
 // window.onload = function(){
 //         listeners;
 //         getuserOrders;
 //  }
-window.onload = getuserOrders;
+window.onload = start;
 // window.onload = listeners;
 
 // $(document).ready(listeners)
   // $(document).ready(getuserOrders)
 
-
+  function start(){
+    getuserOrders();
+    listeners();
+// plan: On the users show page, when "orders" is moused over or clicked the customers list of orders is rendered
 function getuserOrders(){
     var response;
       $.get("/trips.json", function(response){
@@ -20,7 +23,7 @@ function getuserOrders(){
         console.log(response)
         for (let i=0; i< response.length; i++){
           var meals = response[i]["meals"];
-          var date = new Date(response[i]["date"])
+          var date = new Date(response[i]["date"]).toLocaleDateString()
           // console.log("Date: " + date.toLocaleDateString())
           var orderTotal = meals.reduce(function (acc, meal){
             if (!meal){ return parseFloat(0)} else {
@@ -41,32 +44,92 @@ function getuserOrders(){
                // console.log(orderList)
                $("#orders").empty();
                $("#orders").html(orderList);
-
-
-
-          // function meals(){
-          //   response[i]["meals"].forEach(function(meal){
-          //
-          //        `${meal["name"]}  $${meal["price"]}`
-          //       console.log(`${meal["name"]}  $${meal["price"]}`);
-          //      })
-          //    }
-
-
-          // function meals(){
-          //    $.each(response[i]["meals"], function(index, meal){
-          //      debugger
-          //
-          //       return `${meal["name"]}  $${meal["price"]}`
-          //         console.log(`${meal["name"]}  $${meal["price"]}`);
-          //       })
-          //     }
-
-          // var meals = response[i]["meals"]
-
-
-         // console.log(response[i]["meals"])
-
-
          }
-)}
+)};
+///////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////
+// on meal show page - when the 'next' button is pressed, the next meal in the list is loaded
+ function nextMeal(){
+  $(".js-next").on("click", setNext())
+}
+
+function setNext(){
+  var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+  $(".js-next").show();
+   if(nextId){
+
+  $.get("/meals/" + nextId + ".json", function(data){
+    console.log(data["name"])
+     $("#mealName").html(data["name"]);
+     $("#mealDescription").html(data["description"]);
+     $("#mealPrice").html("$" + parseInt(data["price"]).toFixed(2));
+     $("#mealCalories").html(data["calorie_count"]);
+     $(".js-next").attr("data-id", data["id"]);
+     $(".js-prev").attr("data-id", parseInt(data["id"]) - 1 );
+  })
+} else {
+  $(".js-next").hide();
+}
+}
+
+function setPrevious(){
+  var prevId = parseInt($(".js-prev").attr("data-id")) - 1;
+   $(".js-prev").show();
+    console.log($(".js-prev").attr("data-id"));
+   if (prevId){
+
+  $.get("/meals/" + prevId + ".json", function(data){
+    console.log(data["name"])
+     $("#mealName").html(data["name"]);
+     $("#mealDescription").html(data["description"]);
+     $("#mealPrice").html("$" + parseInt(data["price"]).toFixed(2));
+     $("#mealCalories").html(data["calorie_count"]);
+     $(".js-prev").attr("data-id", data["id"]);
+     $(".js-next").attr("data-id", parseInt(data["id"]) + 1);
+  })
+} else {
+  $(".js-prev").hide();
+}
+}
+
+// /////////////////////////////////////////////////////////////////////////
+// Code to display meal description & calorie count to menu when meal name is clicked
+function listeners(){
+  var meals = document.getElementsByClassName("descrip");
+
+  for (i=0; i<meals.length; i++){
+    meals[i].addEventListener("click", getDescription);
+  }
+}
+
+function getDescription(e){
+  url = $(this).attr("href")
+  console.log(url)
+  e.preventDefault();
+  $.get(`${url}.json`, function(response){
+    $(`td.mealDescription-${response["id"]}`).empty();
+    $(`td.mealDescription-${response["id"]}`).html(response["description"] + "<br>  Calorie Count: " + response["calorie_count"]).toggle();
+    // alert(response["description"] + "  Calorie Count: " + response["calorie_count"])
+  })
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// when the word "meal" is clicked the list of meals is rendred on the page
+function getMeals(){
+   $.get("/meals.json", function(response){
+         // console.log("Response: ", response["0"])
+         mealsContainer = document.getElementById("meals-list");
+           for (var i=0; i<response.length; i++){
+            // $("#meals-list ").after("<td>" + response[i]["name"] + "</td><td>" + response[i]["price"] + "</td><br>")
+            var meals;
+            meals += "<tr><td>" + response[i]["name"] + "</td><td>" + "$" + response[i]["price"] + "</td></tr>"
+           }
+           $("#meals-list").append(meals)
+        });
+      }
+
+}
