81ae13e02761a173f62cee75963ab7336c3158ab
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-11-02 17:56:58 -0400

    removed test directory

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 module Restaurant
   class Application < Rails::Application
