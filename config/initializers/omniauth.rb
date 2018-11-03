a4b89902fde3eab06aa8d969780586f11f9c1d72
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-11-03 00:19:12 -0400

    added appid to omniauthrb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,15 @@
 # OmniAuth.config.logger = Rails.logger
 
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+#   # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+# end
+
+# OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-end
+ provider :facebook, Rails.application.secrets.facebook_app_id,
+ Rails.application.secrets.facebook_app_secret
+
 
 # OmniAuth.config.logger = Rails.logger
 # Rails.application.config.middleware.use OmniAuth::Builder do
