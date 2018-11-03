fdc5a0733bb071eac2c3d3f2871fc40832db274b
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-11-02 23:00:17 -0400

    added omiauth to apprb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 require "active_model_serializers"
+require 'omniauth'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
